114cbe270f2dd76f23fa9ac8a65bb0265b632697
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-09-28 14:07:35 -0400

    cart html progress

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from backend.models import ShoppingCart
 
 # Create your views here.
 
 
 def shopping_cart(request):
-    cart = ShoppingCart.objects.get()
     return render(request, "frontend/shopping_cart.html")
