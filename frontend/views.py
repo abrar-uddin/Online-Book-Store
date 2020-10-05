576def04e31d3d54cc383834570b657982a82070
Eric T <etu001@fiu.edu>
2020-10-05 11:04:16 -0400

    URL Work In Progress

--- frontend/views.py
+++ frontend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "E:/ProgramsForSoftwareEngineering/Online-Book-Store/frontend/templates/frontend/base.html")
