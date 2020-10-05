3eb21e766c4c33d88c57311af21621cd3c5077c2
Eric T <etu001@fiu.edu>
2020-10-05 11:23:20 -0400

    URL Work In Progress

--- frontend/views.py
+++ frontend/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, "E:/ProgramsForSoftwareEngineering/Online-Book-Store/frontend/templates/frontend/base.html")
+    return render(request, "frontend/base.html")
