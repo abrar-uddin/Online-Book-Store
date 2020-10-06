55be8eb99e1b4947cb464683700441524b8c55a6
Jesus Torres <Junior@Jesuss-MBP.hsd1.fl.comcast.net>
2020-10-06 17:18:49 -0400

    bookdescriptions

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('backend.urls')),
+    path('', include('frontend.urls')),
 ]
