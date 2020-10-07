4d83dba2d14cfb443e4bfc72c07f9b9b2b84eb4a
Eric T <etu001@fiu.edu>
2020-10-06 20:40:18 -0400

    URL Work In Progress

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.index, name="index")
+    path('home/', views.index, name="index"),
+    path('shopping_cart/', views.index2, name="index2"),
+    path('profile_management/', views.index3, name="index3")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -5,3 +5,10 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "frontend/base.html")
+
+def index2(request):
+    return render(request, "frontend/shopping_cart.html")
+
+
+def index3(request):
+    return render(request, "frontend/profile_management.html")
