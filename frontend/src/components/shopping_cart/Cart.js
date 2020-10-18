0b7a3cb7e7d4b5623d6052f17dcab668adff5a61
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-10-17 21:53:22 -0400

    checkout btn toogle

--- frontend/src/components/shopping_cart/Cart.js
+++ frontend/src/components/shopping_cart/Cart.js
@@ -2,7 +2,7 @@ import React, {Component, Fragment} from 'react';
 import {connect} from "react-redux";
 import PropTypes from 'prop-types';
 import {getCart, getCartTotal, deleteItem, updateItem, deleteCart} from "../../actions/shopping_cart";
-import shopping_cart from "../../reducers/shopping_cart";
+import {Link} from 'react-router-dom';
 
 export class Cart extends Component {
     static propTypes = {
@@ -25,18 +25,27 @@ export class Cart extends Component {
         this.props.updateItem(item_id, new_quantity, book_id, user, price)
     };
 
-    checkOut(e) {
+    checkRequired() {
         let allAreFilled = true;
         document.getElementById("checkout_form").querySelectorAll("[required]").forEach(function (i) {
             if (!allAreFilled) return;
             if (!i.value) allAreFilled = false;
         })
-        if(allAreFilled){
-            window.location.href = "/#/checkout"
-            this.props.deleteCart()
-        }
 
         console.log(allAreFilled)
+        let submit = document.getElementById("checkout_btn")
+        if (allAreFilled) {
+            submit.disabled = false;
+        } else {
+            submit.disabled = true;
+        }
+    }
+
+    // TODO: Check input fields
+    // TODO: Disable checkout button if cart empty
+    checkOut(e) {
+        console.log('delete')
+        this.props.deleteCart()
     };
 
     showShippingField() {
@@ -46,7 +55,7 @@ export class Cart extends Component {
         let shipping_address = document.getElementById("shipping_address");
         let check_box = document.getElementById("shipping_box")
         let is_checked = check_box.checked;
-        if (is_checked == false) {
+        if (is_checked === false) {
             shipping_info.style.visibility = "visible";
             shipping_first_name.required = true;
             shipping_last_name.required = true;
@@ -56,12 +65,13 @@ export class Cart extends Component {
             shipping_first_name.required = false;
             shipping_last_name.required = false;
             shipping_address.required = false;
+            this.checkRequired()
         }
     };
 
     render() {
         return (
-            <div className="container">
+            <div className="container" onLoad={this.checkRequired}>
                 <h2>Cart Items</h2>
                 {this.props.shopping_cart.map(shopping_cart => (
                     <div key={shopping_cart.id} className="row">
@@ -142,47 +152,53 @@ export class Cart extends Component {
                         <div className="row">
                             <h5>Billing Information</h5>
                             <div className="input-field col s6">
-                                <input id="first_name" type="text" className="validate" required></input>
+                                <input id="first_name" type="text" className="validate" onKeyUp={this.checkRequired}
+                                       required></input>
                                 <label htmlFor="first_name">First Name</label>
                             </div>
                             <div className="input-field col s6">
-                                <input id="last_name" type="text" className="validate" required></input>
+                                <input id="last_name" type="text" className="validate" onKeyUp={this.checkRequired}
+                                       required></input>
                                 <label htmlFor="last_name">Last Name</label>
                             </div>
                         </div>
                         <div className="row">
                             <div className="input-field col s12">
                                 <input id="address" type="text"
-                                       className="validate" required></input>
+                                       className="validate" onKeyUp={this.checkRequired} required></input>
                                 <label htmlFor="address">Address</label>
                             </div>
                         </div>
                         <div className="row">
                             <h5>Payment Information</h5>
                             <div className="input-field col s3">
-                                <input id="full_name" type="text" className="validate" required></input>
+                                <input id="full_name" type="text" className="validate" onKeyUp={this.checkRequired}
+                                       required></input>
                                 <label htmlFor="full_name">Full Name</label>
                             </div>
                             <div className="input-field col s9">
                                 <input id="card_number" type="tel"
-                                       className="validate" required></input>
+                                       className="validate" onKeyUp={this.checkRequired} required></input>
                                 <label htmlFor="card_number">Card Number</label>
                             </div>
                         </div>
                         <div className="row">
                             <div className="input-field col s5">
                                 <i className="material-icons prefix">date_range</i>
-                                <input id="exp_date" type="text" className="datepicker" required></input>
+                                <input id="exp_date" type="text" className="datepicker" onChange={this.checkRequired}
+                                       required></input>
                                 <label htmlFor="exp_date">Expiration Date</label>
                             </div>
                             <div className="input-field col s5">
-                                <input id="cvv" type="tel" className="validate" required></input>
+                                <input id="cvv" type="tel" className="validate" onKeyUp={this.checkRequired}
+                                       required></input>
                                 <label htmlFor="cvv">CVV</label>
                             </div>
                             <div className="col s2">
                                 <p>
                                     <label>
-                                        <input id="shipping_box" type="checkbox" onChange={this.showShippingField}/>
+                                        <input id="shipping_box" type="checkbox"
+                                               onChange={this.showShippingField.bind(this)}/>
                                         <span>Shipping address same as billing</span>
                                     </label>
                                 </p>
@@ -193,26 +209,31 @@ export class Cart extends Component {
                             <h5>Shipping Information</h5>
                             <div className="row" visibility="hidden">
                                 <div className="input-field col s6">
-                                    <input id="shipping_first_name" type="text" className="validate" required></input>
+                                    <input id="shipping_first_name" type="text" className="validate"
+                                           onKeyUp={this.checkRequired} required></input>
                                     <label htmlFor="shipping_first_name">First Name</label>
                                 </div>
                                 <div className="input-field col s6">
-                                    <input id="shipping_last_name" type="text" className="validate" required></input>
+                                    <input id="shipping_last_name" type="text" className="validate"
+                                           onKeyUp={this.checkRequired} required></input>
                                     <label htmlFor="shipping_last_name">Last Name</label>
                                 </div>
                             </div>
                             <div className="row">
                                 <div className="input-field col s12">
                                     <input id="shipping_address" type="text"
-                                           className="validate" required></input>
+                                           className="validate" onKeyUp={this.checkRequired} required></input>
                                     <label htmlFor="shipping_address">Address</label>
                                 </div>
                             </div>
                         </div>
-                        <button type="submit" className="btn waves-effect waves-light"
+                        <Link to="/checkout">
+                            <button type="submit" id="checkout_btn"
+                                className="btn waves-effect waves-light pulse"
                                 onClick={this.checkOut.bind(this)}>Checkout
                             <i className="material-icons right">send</i>
                         </button>
+                        </Link>
                     </form>
                 </div>
             </div>
