c481ed8a280f4467efb065156f0788f35b6b0978
Ahmed Uddin <ahmeduddinict@gmail.com>
2020-11-01 11:22:49 -0500

    postgres db update

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'bookstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': "postgres",
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'Pass2020!',
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': "postgres",
+        'USER': 'postgres',
+        'PASSWORD': 'Pass2020!',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
